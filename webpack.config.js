3fd876e424679a5c4b11f9507085427e8a0cc83d
edwinlny <ed.leong89@gmail.com>
2024-03-16 22:20:51 -0400

    build: configured webpack and package.json
    Added devDepencies to package.json and initial webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'build'),
+      publicPath: '/',
+    },
+    compress: true,
+    port: 8080,
+    historyApiFallback: true,
+    proxy: {
+      '/api': 'http://localhost:1111',
+    },
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /.(css|scss)$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+    }),
+  ],
+};
